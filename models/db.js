e3408c13de5148395869870426fb957b199cbb0b
Ammad Jahangir <ammadjahangir88@gmail.com>
2024-01-10 11:04:03 +0500

    Data Connection changed

--- models/db.js
+++ models/db.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-module.exports = mongoose.connect('mongodb://database:27017/test');
+module.exports = mongoose.connect('mongodb+srv://ammadjahangir88:3644899102%24cfd%40@cluster0.zgwcdcf.mongodb.net/test');
