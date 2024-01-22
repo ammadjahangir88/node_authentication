3dffc3b5e1675b4d476855dba5e61e62899eb0f4
Ammad Jahangir <ammadjahangir88@gmail.com>
2024-01-22 15:02:49 +0500

    Latest Changes done in Index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(userRouter)
 app.use(articleRouter)
 app.use(commentRouter)
 app.get('/',(req,resp)=>{
-    resp.send("Hello World")
+    resp.send("Hello Ammad")
 
 })
 app.listen(8000,()=>{
--- models/db.js
+++ models/db.js
@@ -1,2 +1,4 @@
 const mongoose = require('mongoose');
-module.exports = mongoose.connect('mongodb+srv://ammadjahangir88:3644899102%24cfd%40@cluster0.zgwcdcf.mongodb.net/test');
+
+
+module.exports = mongoose.connect('mongodb+srv://ammadjahangir88:090078601@cluster0.zgwcdcf.mongodb.net/test');
